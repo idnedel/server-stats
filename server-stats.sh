c9f31c514665128150ab7ad65fff92c8c34fa567
idnedel <nedel.matheus@gmail.com>
2025-10-01 17:16:17 +0000

    simplified cpu and memory stats and added disk stats

--- server-stats.sh
+++ server-stats.sh
@@ -11,24 +11,33 @@ CpuUsed=$((user + nice + system + irq + softirq + steal))
 CpuData=$((100 * CpuUsed / CpuTotal))
 
 #Memory data
-while read label total used free shared buff_cache available; do
-	if [[ $label == "Mem:" ]]; then
-		MemTotal=$total
-		MemUsed=$used
-		MemFree=$free
-		MemAvailable=$available
-		break
-	fi
-done < <(free -m)
+read label MemTotal MemUsed MemFree shared buff_cache MemAvailable <<< $(free -m | grep 'Mem:')
 
 MemUsageData=$((100 * MemUsed / MemTotal))
 MemFreeData=$((100 * MemFree / MemTotal))
 
 #Disk data
+read Filesystem DiskTotal DiskUsed DiskAvail DiskUsePercent Mounted <<<$(df / | awk 'NR==2')
 
+DiskUsageData=$((100 * DiskUsed / DiskTotal))
+DiskAvailData=$((100 * DiskAvail / DiskTotal))
+
+echo ">>> CPU"
 echo "Total CPU usage: $CpuData%"
-echo "Total memory usage - Used: $MemUsageData% / Free: $MemFreeData%"
-#echo "Total disk usage - Used: / Free: "
-#echo "Top 5 process by CPU usage: "
-#echo "Top 5 process by memory usage: "
+echo " "
 
+echo ">>> MEMORY"
+echo "Total memory usage - Used: $MemUsageData% / Free: $MemFreeData%"
+echo " "
+
+echo ">>> DISK"
+echo "Total disk usage - Used: $DiskUsageData% / Free: $DiskAvailData%"
+echo " "
+
+#echo ">>> CPU PROCESS"
+#echo "Top 5 process by CPU usage: "
+#echo " "
+
+#echo ">>> MEMORY PROCESS"
+#echo "Top 5 process by memory usage: "
+#echo " "
